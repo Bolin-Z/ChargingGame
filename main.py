66abde4d54ca5a5e0768dd00bae03184c7a9f70f
BolinZ <csbolinz@mail.scut.edu.cn>
2025-08-17 06:33:45 +0800

    路径集合计算

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from src.utils import load_network
+from src.utils import load_network, compute_routes
 
 W = load_network("./siouxfalls", "siouxfalls")
+dict_od_to_routes = compute_routes(W, 10)
+print("charging_routes:")
+for od, routes in dict_od_to_routes["charging"].items():
+    o, d = od
+    print(f"\t{o} -> {d}: {len(routes)}")
+print("uncharging_routes:")
+for od, routes in dict_od_to_routes["uncharging"].items():
+    o, d = od
+    print(f"\t{o} -> {d}: {len(routes)}")
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ from math import floor
 import os
 
 import uxsim
+import networkx as nx
+from itertools import islice
 
 
 def load_network(network_dir:str, network_name:str):
@@ -34,6 +36,7 @@ def load_network(network_dir:str, network_name:str):
     # 设置 EVCS 相关参数
     W.EVCS = {
         "charging_periods": settings["charging_periods"],
+        "period_duration": settings["simulation_time"] / settings["charging_periods"],
         "charging_nodes": settings["charging_nodes"],
     }
     # 初始化电价列表
@@ -91,9 +94,74 @@ def load_network(network_dir:str, network_name:str):
     
     return W
 
+def compute_routes(W:uxsim.World, k:int=1):
+    """计算路径集合
+    W: 模拟环境
+    k: 路径数量
+    """
+    
+    # 无充电需求路径集合
+    def enumerate_k_shortest_routes(W:uxsim.World, source:str, target:str, k:int=1):
+        G = nx.DiGraph()
+        link_dict = {}
+        for l in W.LINKS:
+            if not l.attribute["charging_link"]:
+                G.add_edge(l.start_node.name, l.end_node.name, weight=l.length/l.u)
+                link_dict[l.start_node.name, l.end_node.name] = l.name
+        k_shortest_paths = list(islice(nx.shortest_simple_paths(G, W.get_node(source).name, W.get_node(target).name, weight='weight'), k))
+        routes = []
+        for path in k_shortest_paths:
+            route = [link_dict[path[i], path[i + 1]] for i in range(len(path) - 1)]
+            routes.append(route)
+        return routes
+    
+    # 有充电需求路径集合
+    def enumerate_k_shortest_charge_routes(W:uxsim.World, source:str, target:str, k:int=1):
+        G = nx.DiGraph()
+        link_dict = {}
+        for l in W.LINKS:
+            start_node, end_node, weight = l.start_node.name, l.end_node.name, l.length/l.u
+            if not l.attribute["charging_link"]: # 非充电链路
+                # 第一层图
+                G.add_edge("0_" + start_node, "0_" + end_node, weight=weight)
+                link_dict["0_" + start_node, "0_" + end_node] = l.name
+                # 第二层图
+                G.add_edge("1_" + start_node, "1_" + end_node, weight=weight)
+                link_dict["1_" + start_node, "1_" + end_node] = l.name
+            else: # 充电链路
+                G.add_edge("0_" + start_node, "1_" + end_node, weight=weight)
+                link_dict["0_" + start_node, "1_" + end_node] = l.name
+        source_node = "0_" + source
+        target_node = "1_" + target
+        k_shortest_paths = list(islice(nx.shortest_simple_paths(G, source_node, target_node, weight='weight'), k))
+        routes = []
+        for path in k_shortest_paths:
+            route = [link_dict[path[i], path[i + 1]] for i in range(len(path) - 1)]
+            routes.append(route)
+        return routes
+    
+    n_routes_per_od = k
+    od_pairs = set()
+    for veh in W.VEHICLES.values():
+        o = veh.orig.name
+        d = veh.dest.name
+        od_pairs.add((o,d))
+    
+    dict_od_to_routes = {
+        "uncharging": {},
+        "charging": {}
+    }
+    for o, d in od_pairs:
+        dict_od_to_routes["uncharging"][o,d] = enumerate_k_shortest_routes(W, o, d, n_routes_per_od)
+        dict_od_to_routes["charging"][o,d] = enumerate_k_shortest_charge_routes(W, o, d, n_routes_per_od)
+    
+    return dict_od_to_routes
+
 # 获取 t 时刻对应的电价时间段
 def get_period(W:uxsim.World, t:float):
-    return min(floor(t / W.TMAX * W.EVCS["charging_periods"]), W.EVCS["charging_periods"] - 1)
+    return max(0,
+        min(floor(t / W.EVCS["period_duration"]),
+            W.EVCS["charging_periods"] - 1))
 
 # 获取 t 时刻 node 的电价
 def get_price(W:uxsim.World, t:float, node:str):
